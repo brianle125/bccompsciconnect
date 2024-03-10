32719063b6b6b84a3b0975c8334362545553359a
itBeABruhmoment <reee271314@gmail.com>
2024-03-09 16:23:14 -0800

    better name and simplicity

--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { PageBarListComponent } from "../page-bar-list/page-bar-list.component";
+import { BoardList } from "../board-list/board-list.component";
 
 @Component({
     selector: 'app-home-page',
     standalone: true,
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.css',
-    imports: [PageBarListComponent]
+    imports: [BoardList]
 })
 export class HomePageComponent {
 
