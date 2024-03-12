f5252e070c7dbe9e99f00966114f458c154a2c3f
Brian <le.brian125@gmail.com>
2024-03-11 20:09:29 -0700

    Rudimentary backend, database init

--- server/models/db.js
+++ server/models/db.js
@@ -2,17 +2,72 @@ const { Pool } = require('pg');
 require('dotenv').config()
 
 const pool = new Pool({
-    // user:
-    // host:
-    // password:
+    database: 'testing',
+    user: 'postgres',
+    host: 'localhost',
+    password: process.env.LOCAL_PASS
 })
 
 const helpers = {
     //database queries
     init: async function() {
-        const b = 'CREATE TABLE boards IF NOT EXISTS (id integer SERIAL, title character varying(255), created_at timestamp, PRIMARY KEY(id))'
+        //create tables
+        const board = 'CREATE TABLE IF NOT EXISTS boards (id SERIAL, title character varying(255), created_at timestamp, PRIMARY KEY(id))'
+        const topics = 'CREATE TABLE IF NOT EXISTS topics (id SERIAL, boardid integer, question varchar(1000), PRIMARY KEY(id), CONSTRAINT fk_board FOREIGN KEY (boardid) REFERENCES boards(id) ON DELETE CASCADE)'
+        const users = 'CREATE TABLE IF NOT EXISTS users (id SERIAL, username varchar(255), role varchar(255), created_at timestamp, PRIMARY KEY(id))'
+        const messages = 'CREATE TABLE IF NOT EXISTS messages (id SERIAL, topicid integer, body text, status varchar(255), created_at timestamp, PRIMARY KEY(id), CONSTRAINT fk_topic FOREIGN KEY(topicid) REFERENCES topics(id) ON DELETE CASCADE)'
+        const login = 'CREATE TABLE IF NOT EXISTS login (id SERIAL, username varchar(255), password varchar(1000), PRIMARY KEY(id))'
 
+        //call queries
+        await pool.query(board);
+        await pool.query(topics);
+        await pool.query(users);
+        await pool.query(messages);
+        await pool.query(login);
+    },
+
+
+    getBoards: async function() {
+        const res = await pool.query('SELECT * from boards ORDER by id');
+        return res.rows;
+    },
+
+    getBoard: async function(id) {
+        const res = await pool.query(`SELECT * FROM boards WHERE id = $1`, [id]);
+        return res.rows[0];
+    },
+
+    getTopics: async function() {
+        const res = await pool.query('SELECT * from topics ORDER BY id');
+        return res.rows
+    },
+
+    getTopic: async function(id) {
+        const q = 'SELECT * FROM topics WHERE id = $1'
+        const res = await pool.query(q, [id]);
+        return res.rows[0]
+    },
+
+    getMessages: async function(boardId, topicId) {
+        const q = 'SELECT * FROM messages WHERE boardid = $1 AND topicid = $2';
+        const res = await pool.query(q, [topicId]);
+        return res.rows
+    },
+    
+
+
+    addTopic: async function(boardid, question) {
+        //check if board exists
+        try {
+            const precon = await pool.query(`SELECT * from boards WHERE id = ${boardid}`);
+        } catch(err)
+        {
+            console.log('Board does not exist')
+        }
+        const q = 'INSERT INTO topics VALUES(DEFAULT, $1, $2)'
+        const res = await pool.query(q, [boardid, question])
     }
+    
 }
 
 module.exports = { helpers }
--- server/server.js
+++ server/server.js
@@ -7,53 +7,78 @@ var logger = require('morgan');
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
-var cors = require('cors')
+
 var app = express();
+const cors = require('cors')
 const server = require('http').createServer(app)
+const path = require("path");
+const fs = require("fs");
+//for future automatic refreshing
 const io = require('socket.io')(server)
 
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/', express.static(path.join(__dirname, 'public'), { index: ['index.html'] }));
-
+//app.use('/', express.static(path.join(__dirname, 'public'), { index: ['index.html'] }));
+app.use(express.static('../frontend/src'))
 
 // internal modules
 const db = require('./models/db')
 app.use(cors());
+
+//prevent view engine complaints
 app.set('view engine', 'jade');
 
 app.get('/', async (req, res) => {
-  const board = {name: "Bob"};
+  const boards = await db.helpers.getBoards();
+  return res.json(boards)
+})
+
+
+//IN PROGRESS
+// Either id based params (the easy way) or hard coded board topics into url (e.g. /java or /algorithms, etc)
+app.get('/boards/:id', async(req, res) => {
+  let id = req.params.id
+  const board = await db.helpers.getBoard(id)
   res.json(board)
 })
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.get('/boards/:boardId/topic/:topicId', async (req, res) => {
+  let boardId = req.params.boardId;
+  let topicId = req.params.topicId;
+  const topic = await db.helpers.getTopic(topicId);
+  console.log(topic)
+  res.json(topic);
+})
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+//handle topic and message posting
+app.post('/boards/:boardId/topic/:topicId', async (req, res) => {
+  await db.helpers.addTopic(0, 'something');
+  res.redirect('/')
+})
 
 
+//editing
 
-app.listen(port);
+//deleting
+
+
+// Initialize the database
+async function InitDB() {
+  await db.helpers.init()
+  console.log("success")
+}
+
+InitDB().then(() => {
+  app.listen(port);
+  console.log(`Listening on port ${port}`)
+})
+.catch((err) => { console.log(err)})
+
 
 
 
