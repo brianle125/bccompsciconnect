93625837d0b6cf7794bb2dd6404c2b279fe3d840
itBeABruhmoment <reee271314@gmail.com>
2024-03-17 17:39:36 -0700

    ui for listing topics

--- frontend/src/app/board/board.component.ts
+++ frontend/src/app/board/board.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TopicListComponent } from '../topic-list/topic-list.component';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [],
+  imports: [TopicListComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.css'
 })
--- frontend/src/app/topic-list/topic-list.component.ts
+++ frontend/src/app/topic-list/topic-list.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-topic-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './topic-list.component.html',
   styleUrl: './topic-list.component.css'
 })
 export class TopicListComponent {
-
+  @Input() topics: TopicListEntry[] = [new TopicListEntry('topic 1', 'bob', 1, 1, '2023-12-13 1:00pm', '2023-12-13 1:00pm'), new TopicListEntry('topic 1', 'joe', 1, 1, '2023-12-13 1:00pm', '2023-12-13 1:00pm')]
 }
 
 class TopicListEntry {
-
+  constructor(
+    public title: string,
+    public originalPoster: string,
+    public views: number,
+    public numberOfReplies: number,
+    public created: string,
+    public lastUpdated: string
+  ) {}
 }
