51197ea5be218aae3fc05bd6041d0c624c525454
itBeABruhmoment <reee271314@gmail.com>
2024-03-17 17:46:09 -0700

    links in topics

--- frontend/src/app/topic-list/topic-list.component.ts
+++ frontend/src/app/topic-list/topic-list.component.ts
@@ -8,8 +8,14 @@ import { Component, Input } from '@angular/core';
   templateUrl: './topic-list.component.html',
   styleUrl: './topic-list.component.css'
 })
+/**
+ * Component to list topics
+ */
 export class TopicListComponent {
-  @Input() topics: TopicListEntry[] = [new TopicListEntry('topic 1', 'bob', 1, 1, '2023-12-13 1:00pm', '2023-12-13 1:00pm'), new TopicListEntry('topic 1', 'joe', 1, 1, '2023-12-13 1:00pm', '2023-12-13 1:00pm')]
+  @Input() topics: TopicListEntry[] = [
+    new TopicListEntry('topic 1', 'bob', 1, 1, '2023-12-13 1:00pm', '2023-12-13 1:00pm', '/board/0/topic/0'), 
+    new TopicListEntry('topic 1', 'joe', 1, 1, '2023-12-13 1:00pm', '2023-12-13 1:00pm', '/board/0/topic/0')
+  ]
 }
 
 class TopicListEntry {
@@ -19,6 +25,7 @@ class TopicListEntry {
     public views: number,
     public numberOfReplies: number,
     public created: string,
-    public lastUpdated: string
+    public lastUpdated: string,
+    public link: string
   ) {}
 }
