e0559459ab876cc028324e37929fb16024756d25
Jimmy <jimnish2@gmail.com>
2024-03-18 19:27:50 -0700

    created a post for the messages

--- frontend/src/app/board/board.component.ts
+++ frontend/src/app/board/board.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { TopicListComponent } from '../topic-list/topic-list.component';
+import { PostComponent } from '../post/post.component';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [TopicListComponent],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.css'
+  styleUrl: './board.component.css',
+  imports: [TopicListComponent, PostComponent],
 })
 /**
  * The main page for a board
  */
-export class BoardComponent {
-
-}
+export class BoardComponent {}
