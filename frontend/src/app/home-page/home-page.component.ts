ca94aacf95583ed2ac215c493935d7adce87ff2f
itBeABruhmoment <reee271314@gmail.com>
2024-03-19 21:26:04 -0700

    UI for top bar

--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { BoardList } from "../board-list/board-list.component";
+import { TopBarComponent } from '../top-bar/top-bar.component';
 
 @Component({
     selector: 'app-home-page',
     standalone: true,
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.css',
-    imports: [BoardList]
+    imports: [BoardList, TopBarComponent]
 })
 export class HomePageComponent {
 
--- frontend/src/app/top-bar/top-bar.component.ts
+++ frontend/src/app/top-bar/top-bar.component.ts
@@ -1,14 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.css'
 })
 export class TopBarComponent {
-  @Input() loggedIn: boolean = false
+  loggedIn: boolean = true;
+  username: string = 'user'
+  profilePic: string = 'assets/user.png'
+  profileLink: string = ''
+  messagesLink: string = ''
+  logoutLink: string = ''
+  numUnReads: number = 0
 
   constructor(){}
 }
