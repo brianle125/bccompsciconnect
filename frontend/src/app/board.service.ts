fc8f12a9ae8e934c4ee70a0b417bb67a0cb3f28e
itBeABruhmoment <reee271314@gmail.com>
2024-03-20 19:07:18 -0700

    added api call for adding boards and test code

--- frontend/src/app/board.service.ts
+++ frontend/src/app/board.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { api } from './common-strings'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +14,28 @@ export class BoardService {
     temp['BoardService'] = this;
   }
 
-  getBoards() {
-    return this.http.get<any[]>(`${api}/boards`, {withCredentials: true })
+  getBoards(): Observable<BoardResponse[]> {
+    return this.http.get<any[]>(`${api}/boards`, { withCredentials: true }) as Observable<BoardResponse[]>
   }
 
   getBoard(id: any) {
     return this.http.get<any>(`${api}/board/${id}`, {withCredentials: true })
+  }
+
+  addBoard(title: string, description: string, ordering: number) {
+    console.log(`${api}/board`)
+    this.http.post(`${api}/board`, {'boardTitle': title, 'boardDescription': description, 'ordering': ordering}).subscribe({
+      next:(res) => { console.log(res) },
+      error:(e) => { console.log(e) }
+    })
+  }
+
+  addSampleBoards() {
+    // for testing
+    this.addBoard('Courses', 'Discuss course related problems.', 0)
+    this.addBoard('Projects', 'Discuss your code projects.', 1)
+    this.addBoard('Questions', 'Ask and answer any CS related questions.', 2)
+    this.addBoard('OpenSource', 'Discuss open source projects.', 3)
   }
 
   //probably move this to another service file if needed
@@ -27,4 +44,11 @@ export class BoardService {
   }
 
   
+}
+
+export interface BoardResponse {
+  title: string,
+  description: string,
+  id: string,
+  ordering: string
 }
