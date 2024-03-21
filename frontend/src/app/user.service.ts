76a5926e6de67f67fabce153a626306476403aed
itBeABruhmoment <reee271314@gmail.com>
2024-03-20 17:07:53 -0700

    store api url in same place

--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -1,23 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { api } from './common-strings';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  api = 'http://localhost:8080/api';
+  // api = 'http://localhost:8080/api';
   constructor(private http: HttpClient) { }
 
   getUser(username: any) {
-    return this.http.get(`${this.api}/usercheck?name=${username}`, {withCredentials:true})
+    return this.http.get(`${api}/usercheck?name=${username}`, {withCredentials:true})
   }
 
   addUser(user: any) {
-    return this.http.post<any>(`${this.api}/register`, user, {observe: 'response', withCredentials: true});
+    return this.http.post<any>(`${api}/register`, user, {observe: 'response', withCredentials: true});
   }
 
   loginUser(user: any) {
-    return this.http.post<any>(`${this.api}/login`, user, {observe: 'response', withCredentials: true});
+    return this.http.post<any>(`${api}/login`, user, {observe: 'response', withCredentials: true});
   }
 }
