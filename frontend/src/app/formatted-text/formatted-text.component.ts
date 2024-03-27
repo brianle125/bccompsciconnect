f0a278a5216bb0b23338c2df6bf2988e09d2a173
itBeABruhmoment <reee271314@gmail.com>
2024-03-26 21:32:24 -0700

    experimented with displaying formatted text

--- frontend/src/app/formatted-text/formatted-text.component.ts
+++ frontend/src/app/formatted-text/formatted-text.component.ts
@@ -13,8 +13,50 @@ import { marked } from 'marked';
   styleUrl: './formatted-text.component.css'
 })
 export class FormattedTextComponent {
-  @Input() text: string = `text<div>text</div>super <span>special</span> text <markdown>helpme</markdown>`
+  @Input() text: string = `text<div>text</div>super <span>special</span> text <markdown>helpme <button></button></markdown> <button></button><a href="javascript:alert('Hello, world!')"></a>`
 
+  
+
+  // private HTMLAllowedTags: string[] = ['MARKDOWN']
+
+  // private isJavascriptCode(code: string): boolean {
+  //   try {
+  //     new Function(code)
+  //   } catch(e) {
+  //     return false
+  //   }
+  //   return true
+  // }
+
+  // private removeBannedTags(node: ChildNode) {
+  //   let toRemove: ChildNode[] = []
+  //   node.childNodes.forEach((child) => {
+  //     let name: string = child.nodeName
+  //     if(name == 'SCRIPT' || name == 'BUTTON') {
+  //       toRemove.push(child);
+  //     }
+  //     if(name == 'A') {
+  //       let anchor: HTMLAnchorElement = node as unknown as HTMLAnchorElement
+  //       if(this.isJavascriptCode(anchor.href)) {
+  //         toRemove.push(child)
+  //       }
+  //     }
+  //   })
+  //   toRemove.forEach((remove) => {
+  //     node.removeChild(remove)
+  //   })
+  // }
+
+  // private HTMLNodesToText(node: ChildNode) {
+  //   let originalNodes: ChildNode[] = []
+  //   node.childNodes.forEach((child) => {
+  //     originalNodes.push(child)
+  //   })
+  // }
+
+  // private isHTMLAllowedTag(tag: string) {
+  //   return this.HTMLAllowedTags.includes(tag)
+  // }
 
   private nodeToString(node: ChildNode): string {
     let temp = document.createElement("div")
@@ -26,48 +68,62 @@ export class FormattedTextComponent {
 
   private async editDOM(node: ChildNode): Promise<void> {
     // breath first traversal
-    let exploreNext: Queue<NodeAndDepth> = new Queue()
-    exploreNext.push({'node': node, 'depth': 0})
+    let exploreNext: Queue<DOMTraverseNode> = new Queue()
+    exploreNext.push(new DOMTraverseNode(node, 0, false))
 
     while (!exploreNext.isEmpty()) {
-      let nodeAndDepth: NodeAndDepth = exploreNext.pop()
-      console.log(nodeAndDepth)
+      let currentNode: DOMTraverseNode = exploreNext.pop()
+      // this.removeBannedTags(currentNode.node)
+      console.log(currentNode)
       // node's children haven't been accessed yet so editing node's children is fine
-      if(nodeAndDepth.node.nodeName == 'MARKDOWN') {
-        let asHTML: HTMLElement = nodeAndDepth.node as HTMLElement
+      if(currentNode.node.nodeName == 'MARKDOWN') {
+        let asHTML: HTMLElement = currentNode.node as HTMLElement
         console.log('str', asHTML.innerHTML);
-        let compiledMarkdown: string = await marked(this.nodeToString(nodeAndDepth.node));
+        let compiledMarkdown: string = await marked(this.nodeToString(currentNode.node));
         asHTML.innerHTML = compiledMarkdown
         console.log('md', compiledMarkdown)
       }
       
-      nodeAndDepth.node.childNodes.forEach((node) => {
-        if(nodeAndDepth.depth < 10) {
+      currentNode.node.childNodes.forEach((node) => {
+        if(currentNode.depth < 10) {
+          // let allowHTML: boolean = currentNode.htmlAllowed
+          // if(this.isHTMLAllowedTag(currentNode.node.nodeName)) {
+          //   allowHTML = true
+          // }
           console.log('child', node)
-          exploreNext.push({'node': node, 'depth': nodeAndDepth.depth + 1})
+          exploreNext.push(new DOMTraverseNode(node, currentNode.depth + 1, false))
         }
       })
     }
   }
 
   constructor() {
-    let div: HTMLDivElement = document.createElement('div')
-    div.innerHTML = this.text
-    console.log(div.childNodes)
-    console.log(div)
-    div.childNodes.forEach((val) => {
-      console.log(val.textContent, val.nodeName)
-    })
+    // const clean = sanitizeHtml(this.text, this.sanitizeOptions)
+    // sanitizeHtml.default(this.text, this.sanitizeOptions)
+    console.log('clean')
+    // let div: HTMLDivElement = document.createElement('div')
+    // div.innerHTML = this.text
+    // console.log(div.childNodes)
+    // console.log(div)
+    // div.childNodes.forEach((val) => {
+    //   console.log(val.textContent, val.nodeName)
+    // })
 
-    console.log('test')
-    this.editDOM(div).finally(()=> {
-      console.log('result', div.innerHTML)
-    })
+    // console.log('test')
+    // this.editDOM(div).finally(()=> {
+    //   console.log('result', div.innerHTML)
+    //   console.log(document.createTextNode('<div></div>'))
+    // })
+    
     
   }
 }
 
-interface NodeAndDepth {
-  node: ChildNode,
-  depth: number
+class DOMTraverseNode {
+  constructor(
+    public node: ChildNode,
+    public depth: number,
+    public htmlAllowed: boolean
+  ) { }
+  
 }
