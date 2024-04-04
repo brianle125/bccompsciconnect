fe731caf0952bfc38810b000aefa2cf7163975e8
Brian <le.brian125@gmail.com>
2024-04-03 19:09:59 -0700

    Users can only edit their own profile

---
+++ frontend/src/app/session.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { UserService } from './user.service';
+import { Observable, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class sessionGuard implements CanActivate {
+  constructor(private userService: UserService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.userService.isLoggedIn().pipe(
+      map((data: any) => {
+        const username = route.paramMap.get('username');
+        if (data.user === username) {
+          console.log('The session user can edit')
+          return true; 
+        } else {
+          console.log('Cannot edit another profile')
+          this.router.navigate(['/']); 
+          return false;
+        }
+      })
+    );
+  }
+}
